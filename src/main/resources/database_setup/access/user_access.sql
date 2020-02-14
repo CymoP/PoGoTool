c209e299af253905177e6d986b5f975009167d38
CymoP <p1620013x@my365.dmu.ac.uk>
2020-02-14 10:33:01 +0000

    Update user access sql to provide more useful statements

--- src/main/resources/database_setup/access/user_access.sql
+++ src/main/resources/database_setup/access/user_access.sql
@@ -1,1 +1,3 @@
+CREATE USER 'dbuser'@'localhost' IDENTIFIED BY 'B3@fcake9192';
+GRANT ALL PRIVILEGES ON pokemondb.* TO 'dbuser'@'localhost';
 ALTER USER 'dbuser'@'localhost' IDENTIFIED WITH mysql_native_password BY 'B3@fcake9192';
