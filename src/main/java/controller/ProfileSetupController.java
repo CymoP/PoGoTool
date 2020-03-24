8aa80192cac8b31dbe90cce2941e1345a02e5af2
CymoP <p1620013x@my365.dmu.ac.uk>
2020-03-24 15:42:17 +0000

    Creating initial profile setup UI

---
+++ src/main/java/controller/ProfileSetupController.java
@@ -0,0 +1,37 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import services.UserService;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ProfileSetupController implements Initializable {
+
+    @FXML
+    public Button submitButton;
+
+    @FXML
+    public CheckBox battleSimulatorCheckBox;
+
+    @FXML
+    public CheckBox userMaintenanceCheckBox;
+
+    @FXML
+    public CheckBox dataMaintenanceCheckBox;
+
+    private UserService userService = UserService.getInstance();
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        if (!userService.checkLoggedInUserIsAdmin()) {
+            userMaintenanceCheckBox.setManaged(false);
+            dataMaintenanceCheckBox.setManaged(false);
+            userMaintenanceCheckBox.setVisible(false);
+            dataMaintenanceCheckBox.setVisible(false);
+        }
+    }
+}
